c045af70e5b1e6364354de036e45e2d574db50e7
Che B <cheb@Ches-MacBook-Air.local>
2016-11-16 09:27:02 -0500

    Movie app

---
+++ movie_ajax/movie.js
@@ -0,0 +1,15 @@
+var app = angular.module('movie', ['ui.router']);
+
+app.controller('MovieController', function($scope, $http) {
+
+});
+
+$scope.che = function() {
+  console.log("check");
+};
+
+// app.config(function($stateProvider, $urlRouterProvider) {
+//   .state{
+//
+//   }
+// });
